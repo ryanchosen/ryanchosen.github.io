232b4929e20f442644d09610c855624fde54fc96
Ryan Su <563607356@qq.com>
2025-05-27 16:12:31 +0800

    deploy

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "Blog111",
+  title: "Blog",
   description: "My Blog",
   theme,
   head: [
@@ -14,25 +14,30 @@ export default defineUserConfig({
       "script",
       {},
       `\
-        // 创建一个 loader 元素并插入到 body
         (function() {
-          console.log('我运行了')
-          var loader = document.createElement('div');
-          loader.className = 'loader';
-          document.body.appendChild(loader);
-          
-          var loader1 = document.createElement('div');
-          loader1.className = 'custom-loader-overlay';
-          document.body.appendChild(loader1);
+            function run() {
+              var loader = document.createElement('div');
+              loader.className = 'loader';
+              document.body.appendChild(loader);
+              
+              var loader1 = document.createElement('div');
+              loader1.className = 'custom-loader-overlay';
+              document.body.appendChild(loader1);
 
-          setTimeout(function() {
-            loader.classList.add('fade-out');
-            loader1.classList.add('fade-out');
-            setTimeout(function() {
-              if (loader && loader.parentNode) loader.parentNode.removeChild(loader);
-              if (loader1 && loader1.parentNode) loader1.parentNode.removeChild(loader1);
-            }, 200); // 等待过渡动画结束
-          }, 1200);
+              setTimeout(function() {
+                loader.classList.add('fade-out');
+                loader1.classList.add('fade-out');
+                setTimeout(function() {
+                  if (loader && loader.parentNode) loader.parentNode.removeChild(loader);
+                  if (loader1 && loader1.parentNode) loader1.parentNode.removeChild(loader1);
+                }, 200); // 等待过渡动画结束
+              }, 1200); 
+            }
+            if (document.readyState === 'loading') {
+              document.addEventListener('DOMContentLoaded', run);
+            } else {
+              run();
+            }
         })();
       `,
     ],
